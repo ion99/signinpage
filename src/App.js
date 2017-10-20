d80843b2f4993710d48a22434c6a6584eccfb7e6
Ion Gorincioi <gorincioii@yahoo.com>
2017-10-20 14:58:45 -0500

    Make the App component pure declarative. No DOM manipulations.

--- src/App.js
+++ src/App.js
@@ -5,15 +5,14 @@ const inputButtonStyle = {
   margin: '5px'
 };
 
-const id = () => Math.floor(Math.random() * 10000);
-
 class App extends Component {
   constructor() {
     super();
     this.state = {
       emailInputValue: '',
       passwordInputValue: '',
-      credenciales: {}
+      credenciales: {},
+      showSuccess: false
     };
     
     this.updateEmailInputValue = this.updateEmailInputValue.bind(this);
@@ -33,15 +32,16 @@ class App extends Component {
   } 
 
   onSignIn() {
+    const id = () => Math.floor(Math.random() * 10000);
 
-    this.setState({ 
-      credenciales: Object.assign(this.state.credenciales, {id:  id(), user: this.state.emailInputValue, userPassword: this.state.passwordInputValue})
-    });
+    if (this.state.emailInputValue !== "" && this.state.passwordInputValue !== "") {
+      this.setState({
+        showSuccess: !this.state.showSuccess, 
+        credenciales: Object.assign(this.state.credenciales, {id:  id(), user: this.state.emailInputValue, userPassword: this.state.passwordInputValue})
+      });
+    }; 
 
-    const userName = this.state.emailInputValue.slice(0, this.state.emailInputValue.indexOf("@"));
-    
-    document.querySelector("form").style.display = "none";
-    document.querySelector("h1").innerHTML = `Successfully Signed In ${userName}!`;
+    //const userName = this.state.emailInputValue.slice(0, this.state.emailInputValue.indexOf("@"));
     
     console.log('credentiales: ', this.state);
   } 
@@ -50,17 +50,26 @@ class App extends Component {
 
     return (
       <div className="App">
-        <h1 ref="greet">Hello World!</h1>
-        <form ref="signIn">
+        <h1>Hello World!</h1>
+        <form>
           <input type="text" id="useremail" placeholder="Email Address..." value={this.state.emailInputValue} onChange={this.updateEmailInputValue} style={inputButtonStyle} />
           <br/>
           <input type="password" id="password" placeholder="Password..." value={this.state.passwordInputValue} onChange={this.updatePasswordInputValue} style={inputButtonStyle} />
           <br/>
-          <button id="button" type="button" className="btn btn-primary" onClick={ () => this.onSignIn() }>Sign In</button>
+          <button id="button" type="button" className="btn btn-primary" onClick={ () => {this.onSignIn()} }>Sign In</button>
         </form>
+        {this.state.showSuccess && <Success />}
       </div> 
     );
   }
+}
+
+const Success = () => {
+  return (
+    <div>
+      <h1>Successfully Signed In!</h1>
+    </div>
+  )
 }
 
 export default App;
